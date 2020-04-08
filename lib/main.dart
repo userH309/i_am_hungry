19e6065e1b7e9d0f40d1fea4466fe39c9bcd7853
erik.andresen@shortcut.no <erik.andresen@shortcut.no>
2020-04-08 12:27:17 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black54,
+        appBar: AppBar(
+          backgroundColor: Colors.greenAccent[700],
+          title: Text(
+            "I am hungry",
+          ),
+        ),
+        body: Column(
+          children: <Widget>[
+            Image(
+              image: NetworkImage(
+                "https://miro.medium.com/max/794/0*4Wsh7KHc5WndFFTT.jpg",
+              ),
+            ),
+            Image(
+              width: 100,
+              height: 100,
+              image: AssetImage(
+                "images/lol_man.png",
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
